56d7fc28486b58ee2381575b83c1f58f1bc63df9
thomas-rooty <essainono1@gmail.com>
2021-10-12 13:23:26 +0200

    added shell cmd

---
+++ dev.sh
@@ -0,0 +1,2 @@
+docker-compose -f .\docker-compose.yml up -d --build
+echo "Dev Rocket launched on localhost:3001"
---
+++ prod.sh
@@ -0,0 +1,2 @@
+docker-compose -f .\docker-compose.prod.yml up -d --build
+echo "Prod Rocket launched on localhost:1337 with Nginx"
