25c232357c864e5b43dcaa10bb456c460b4794c2
thomas-rooty <essainono1@gmail.com>
2021-10-13 20:42:40 +0200

    added bootstrap

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Login.css";
 
 export default function Login() {
@@ -12,8 +13,9 @@ export default function Login() {
   }
 
   function handleSubmit(event) {
-    console.log("Submitted :");
-    console.log("Email : " + email + " Password : " + password);
+    console.log("Submitted inputs by user :");
+    console.log("Email : " + email);
+    console.log("Password : " + password);
     event.preventDefault();
   }
 
