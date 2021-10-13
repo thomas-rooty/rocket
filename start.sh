1bcaa74a50e254ff59ff06c44aee995597345ed3
thomas-rooty <essainono1@gmail.com>
2021-10-13 15:30:51 +0200

    changed container name

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-docker build -t rocket:rocket .
-docker run -d -it --rm -v ${PWD}:/app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocket:rocket
+docker build -t rocketrocket-app:rocket-app .
+docker run -d -it --rm -v ${PWD}:/app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocketrocket-app:rocketrocket-app
 echo "Rock.ET built and deployed at http://localhost:3001"
