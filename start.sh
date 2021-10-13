3b102e694fd875e75596632cd1c46c1c643235cc
thomas-rooty <essainono1@gmail.com>
2021-10-13 15:37:18 +0200

    corected

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-docker build -t rocketrocket-app:rocket-app .
-docker run -d -it --rm -v ${PWD}:/app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocketrocket-app:rocketrocket-app
+docker build -t rocket-app:rocket-app .
+docker run -d -it --rm -v ${PWD}:/app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocket-app:rocket-app
 echo "Rock.ET built and deployed at http://localhost:3001"
