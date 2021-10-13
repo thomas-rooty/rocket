5de35beacb2ae914ff2560f663817b36713aedf2
thomas-rooty <essainono1@gmail.com>
2021-10-13 14:36:31 +0200

    renamed app

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker build -t rocket:rocket .
-docker run -it --rm -v ${PWD}:/app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocket:rocket-app
+docker run -it --rm -v ${PWD}:/app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocket:rocket
