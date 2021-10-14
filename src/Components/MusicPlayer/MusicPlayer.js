40196f9b6e0264b3f66ef19669ad01b8d15897a0
thomas-rooty <essainono1@gmail.com>
2021-10-14 09:23:03 +0200

    added armand's work

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -13,10 +13,57 @@ import PauseIcon from '@mui/icons-material/Pause';
 
 // api
 
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import SliderMui from '../slider/Slider';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+
 import getMusic from '../../api/getMusic';
 
 // DEV
+function secondsToHms(seconds) {
+  if (!seconds) return '00m 00s'
 
+  let duration = seconds
+  let hours = duration / 3600
+  duration = duration % 3600
+
+  let min = parseInt(duration / 60)
+  duration = duration % 60
+
+  let sec = parseInt(duration)
+
+  if (sec < 10) {
+    sec = `0${sec}`
+  }
+  if (min < 10) {
+    min = `0${min}`
+  }
+
+  if (parseInt(hours, 10) > 0) {
+    return `${parseInt(hours, 10)}h ${min}m ${sec}s`
+  } else if (min == 0) {
+    return `00m ${sec}s`
+  } else {
+    return `${min}m ${sec}s`
+  }
+}
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{secondsToHms(props.duration)}</Typography>
+      </Box>
+    </Box>
+  );
+}
 class MusicPlayer extends Component{
   constructor(props) {
     super(props);
@@ -24,29 +71,47 @@ class MusicPlayer extends Component{
       musicIndex : 0,
       loadedDataMusic : false,
       play : false,
-      audio : [],
-      data : []
+      audio : new Audio(),
+      data : [],
+      firstLog : false,
+      reload : false,
+      duration : 0,
+      timePlayed : 0,
+      volume : 100,
     }
+    this.myRef = React.createRef()
     this.killAudio = this.killAudio.bind(this);
     this.UpIndex = this.UpIndex.bind(this);
     this.DownIndex = this.DownIndex.bind(this);
     this.loop = this.loop.bind(this);
     this.PlayMusic = this.PlayMusic.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount(){
     getMusic().then(
       result => this.setState({
         data : result,
-        audio : new Audio(result[0]["mp3"])
+        firstLog : true,
+      }, () => {
+        this.state.audio.setAttribute('src', result[this.state.musicIndex]['mp3'])
+        this.state.audio.addEventListener('loadedmetadata', (e) => {
+          this.setState({
+            duration : e.target.duration
+          })
+        });
       })
     )
   }
    killAudio(x){
      this.state.audio.pause()
      this.setState({
-       musicIndex : x,
        play : true,
-       audio : new Audio(this.state.data[x]["mp3"])
+       musicIndex : x,
+     }, () => {
+       console.log(this.state.audio)
+       this.state.audio.setAttribute('src', this.state.data[this.state.musicIndex]['mp3'])
+       this.state.audio.play()
      })
   }
    UpIndex(){
@@ -77,14 +142,40 @@ class MusicPlayer extends Component{
       })
     }
   }
-  render(){
-    console.log(this.state.data)
-    if (this.state.play === true) {
-      this.state.audio.play()
+    handleChange(e){
+      this.setState({
+        volume : e.target.value,
+      })
     }
+    onChange(e) {
+      var x = (this.state.audio.duration / 100) * e.target.value
+      this.state.audio.currentTime = x
+      this.setState({
+        timePlayed : x
+      })
+    }
+   render(){
+     setTimeout(() => {
+     if (this.state.play){
+       this.setState({
+         timePlayed : this.state.audio.currentTime
+       })
+     }
+    }, 500);
+    if (this.state.firstLog) {
+      if (this.state.play === true) {
+        this.state.audio.play()
+      } else {
+        this.state.audio.pause()
+      }
+    }
+    this.state.audio.volume = this.state.volume / 100
     return (
+
       <div className="player">
-        <div className="picturesPlayer">
+          <div className="picturesPlayer" style={{
+            backgroundImage : this.state.data[this.state.musicIndex] && `url(${this.state.data[this.state.musicIndex]['Pictures']})`
+          }}>
           <div className="NameSong">
             <h5>
             {
@@ -96,19 +187,32 @@ class MusicPlayer extends Component{
         </div>
         <div className="ContentPlayer">
           <div className="ProgressBarPlayer">
+            <SliderMui percentage={this.state.timePlayed / this.state.duration * 100} onChange={this.onChange}/>
           </div>
           <div className="indicatorPlayer">
             <ReplayIcon onClick={this.loop}/>
             <SkipPreviousIcon
-              onClick={this.DownIndex}/>
+              onClick={this.DownIndex}
+              style = {{
+                color : this.state.musicIndex -1 < 0 && '#F8F8F8'
+              }}/>
             {
               !this.state.play
               ? <PlayArrowIcon onClick={() => {this.PlayMusic('play')}}/>
             : <PauseIcon onClick={() => {this.PlayMusic('pause')}}/>
             }
 
-            <VolumeUpIcon/>
-            <SkipNextIcon onClick={this.UpIndex}/>
+            <Box sx={{ width: 200 }}>
+              <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+                <VolumeDown />
+                <Slider aria-label="Volume" value={this.state.volume} onChange={this.handleChange} />
+                <VolumeUp />
+              </Stack>
+            </Box>
+            <SkipNextIcon onClick={this.UpIndex}
+              style = {{
+                color : this.state.musicIndex +1 > this.state.data.length -1 && '#F8F8F8'
+              }}/>
           </div>
         </div>
       </div>
--- windows-start.bat
+++ windows-start.bat
@@ -1,3 +1,3 @@
 docker build -t rocket:rocket .
-docker run -d -it --rm -v ${PWD}:/app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocket:rocket
+docker run -d -it --rm -v /app -v /app/node_modules -p 3001:3000 -e CHOKIDAR_USEPOLLING=true rocket:rocket
 echo "Rock.ET built and deployed at http://localhost:3001"
