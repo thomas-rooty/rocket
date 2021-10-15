cf21abedbd28aa08b78cd87217d3a534c36681df
thomas-rooty <essainono1@gmail.com>
2021-10-15 11:52:19 +0200

    added landing page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MusicPlayer from './Components/MusicPlayer/MusicPlayer';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
+import LandingPage from './Components/LandingPage/LandingPage';
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,7 +20,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <PublicRoute restricted={false} exact path="/" component={sayHello}/>
+        <PublicRoute restricted={false} exact path="/" component={LandingPage}/>
         <PublicRoute restricted={false} exact path="/register" component={Register}/>
         <PublicRoute restricted={true} exact path="/login" component={Login}/>
         <PrivateRoute exact path="/player" component={MusicPlayer}/>
